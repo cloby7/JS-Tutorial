3cbf3225bda0f61a3458df3954a107269e89f8cd
cloby7 <anuradhabsnk@gmail.com>
2023-06-02 21:40:11 +0530

    variables

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
  //Comment
- console.log('Hello world');
+//  console.log('Hello world');
+
+//--------------Variables----------------
+
+// let name = 'Anu';
+// console.log(name);
+
+// //cammol notation - best practice -> fistName
+// let firstName,  surName;
+
+// //best practice
+// // let firstName;
+// // let surName;
+
+
+//--------------Constants----------------
+
+
